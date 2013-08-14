59ca4dd59beda8ea30b9c8697537149b2381ad72
Nino Ninux <nino.ninux@gmail.com>
2013-08-14 22:36:45 +0200

    started README

---
+++ README.m
@@ -0,0 +1,11 @@
+PY-uLCD
+======
+
+This project shall show some applications for the 4D Systems uLCD-32PT 
+programmed with Python.
+
+Troubles
+==
+
+The implementation for serial communications fails on python3. So for this
+project only python2.7 will be used.
