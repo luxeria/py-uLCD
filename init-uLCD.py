5ace48156b77c9be1d17e4497f272c373f014e34
Nino Ninux <nino.ninux@gmail.com>
2013-08-14 23:07:36 +0200

    created a simple init-program

---
+++ init-uLCD.py
@@ -0,0 +1,30 @@
+# print out a message what's going to happen 
+print 'Starting initialisation for uLCD...'
+
+# import the needed extensions for python
+import serial
+import time
+
+# open the serial connection (check with dmesg if it's not working)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+# write the command for AUTOBAUD by sending the character 'U'
+ser.write('U')
+time.sleep(1)
+
+# write the command for ERASE by sending the character 'E'
+ser.write('E')
+time.sleep(1)
+
+# write the welcome-scentense
+# Byte 1: column = 0
+# Byte 2: row = 0
+# Byte 3: font = 0
+# Byte 4 & 5: color = ffff
+# Byte 5+: "string"
+# Byte n: terminator = 0
+ser.write('s\x00\x00\x00\xff\xffHello Geek, you are using py-uLCD!\x00')
+time.sleep(1)
+
+# write out a message that the sequence is done
+print 'Initialisation for uLCD is done.'
