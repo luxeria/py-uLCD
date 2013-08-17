6ac6639b5062a68d6e27a54cdc92a67148bf980c
daniw <daniel.winz@stud.hslu.ch>
2013-08-17 13:49:53 +0200

    skripts für den Download der Datenblätter erstellt

---
+++ datasheets/PICASO-SGC-COMMANDS-SIS-rev3.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+# List of the datasheets:
+
+# Command Set for the PICASO-SGC
+wget http://www.4dsystems.com.au/downloads/Semiconductors/PICASO-SGC/Docs/PICASO-SGC-COMMANDS-SIS-rev3.pdf
